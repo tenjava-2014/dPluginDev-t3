efcb97d218d6de40902bfd8d5091cc04b12d22df
dPluginDev <mchungerdeans@gmail.com>
2014-07-12 10:55:24 -0700

    Playing with randoms

--- src/main/java/com/tenjava/entries/dPluginDev/t3/Config.java
+++ src/main/java/com/tenjava/entries/dPluginDev/t3/Config.java
@@ -1,9 +1,13 @@
 package com.tenjava.entries.dPluginDev.t3;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
 public class Config {
 	
 	public static int eventChance = 5;
 	public static int eventRepeatDelay = 300;
-	public static int spawnRadius = 500;
-	public static String enabledWorld = "String";
+	public static int meteorSpawnRadius = 500;
+	public static int meteorSpawnHeight = 300;
+	public static World enabledWorld = Bukkit.getServer().getWorld("world");
 }
--- src/main/java/com/tenjava/entries/dPluginDev/t3/MeteorStrikeEvent.java
+++ src/main/java/com/tenjava/entries/dPluginDev/t3/MeteorStrikeEvent.java
@@ -2,14 +2,19 @@ package com.tenjava.entries.dPluginDev.t3;
 
 import java.util.Random;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 
 public class MeteorStrikeEvent {
 	
-	Random rX = new Random();
-	Random rZ = new Random();
+	private static Random rand = new Random();
 
+	@SuppressWarnings("deprecation")
 	public static void strikeMeteor(World world){
-		
+		int meteorX = rand.nextInt((Config.meteorSpawnRadius - world.getSpawnLocation().getBlockX()) + 1) + world.getSpawnLocation().getBlockX();
+	    int meteorZ = rand.nextInt((Config.meteorSpawnRadius - world.getSpawnLocation().getBlockZ()) + 1) + world.getSpawnLocation().getBlockZ();
+	    
+	    world.spawnFallingBlock(new Location(world, (double) meteorX, (double) Config.meteorSpawnHeight, (double) meteorZ), Material.STONE, (byte) 0);
 	}
 }
