3572d481ecb27f93fcc11b8415e066863d2267ae
dPluginDev <mchungerdeans@gmail.com>
2014-07-12 11:39:25 -0700

    Beginning sphere generation

--- src/main/java/com/tenjava/entries/dPluginDev/t3/MeteorStrikeEvent.java
+++ src/main/java/com/tenjava/entries/dPluginDev/t3/MeteorStrikeEvent.java
@@ -1,5 +1,7 @@
 package com.tenjava.entries.dPluginDev.t3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.bukkit.Location;
@@ -15,7 +17,24 @@ public class MeteorStrikeEvent {
 		int meteorX = rand.nextInt((Config.meteorSpawnRadius - world.getSpawnLocation().getBlockX()) + 1) + world.getSpawnLocation().getBlockX();
 	    int meteorZ = rand.nextInt((Config.meteorSpawnRadius - world.getSpawnLocation().getBlockZ()) + 1) + world.getSpawnLocation().getBlockZ();
 	    
-	    world.spawnFallingBlock(new Location(world, (double) meteorX, (double) Config.meteorSpawnHeight, (double) meteorZ), Material.STONE, (byte) 0);
+	    //world.spawnFallingBlock(new Location(world, (double) meteorX, (double) Config.meteorSpawnHeight, (double) meteorZ), Material.STONE, (byte) 0);
 	    
+	}
+	
+	public static List<Location> generateSphere(Location centerBlock, int radius, boolean hollow){
+		List<Location> circleBlocks = new ArrayList<Location>();
+		int bX = centerBlock.getBlockX();
+		int bY = centerBlock.getBlockY();
+		int bZ = centerBlock.getBlockZ();
+		
+		for (int x = bX - radius; x <= bX + radius; x++){
+			for (int y = bY - radius; y <= bY + radius; y++){
+				for (int z = bZ - radius; z <= bZ + radius; z++){
+					
+				}
+			}
+		}
+		
+		return circleBlocks;
 	}
 }
