f217ab071be835797d2c014473711a87df2057ac
dPluginDev <mchungerdeans@gmail.com>
2014-07-12 09:08:12 -0700

    First commit, was having issues with Git/Maven

---
+++ src/main/java/com/tenjava/entries/dPluginDev/t3/RandomEvents.java
@@ -0,0 +1,7 @@
+package com.tenjava.entries.dPluginDev.t3;
+
+import org.bukkit.event.Listener;
+
+public class RandomEvents implements Listener {
+
+}
--- src/main/java/com/tenjava/entries/dPluginDev/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/dPluginDev/t3/TenJava.java
@@ -1,5 +1,19 @@
 package com.tenjava.entries.dPluginDev.t3;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+	
+	static PluginManager pm = Bukkit.getServer().getPluginManager();
+	
+	public void onEnable(){
+		System.out.println("Plugin has been successfully enabled!");
+		pm.registerEvents(new RandomEvents(), this);
+	}
+	
+	public void onDisable(){
+		System.out.println("Plugin has been successfully disabled!");
+	}
+}
