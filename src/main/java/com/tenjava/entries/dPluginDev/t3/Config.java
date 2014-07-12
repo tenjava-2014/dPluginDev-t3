8b28e813e777f90a23ea00ee8bebd92a80a51312
dPluginDev <mchungerdeans@gmail.com>
2014-07-12 14:29:26 -0700

    Just stuff

--- src/main/java/com/tenjava/entries/dPluginDev/t3/Config.java
+++ src/main/java/com/tenjava/entries/dPluginDev/t3/Config.java
@@ -8,7 +8,7 @@ public class Config {
 	public static int eventChance = 5;
 	public static int eventRepeatDelay = 300;
 	public static int meteorSpawnRadius = 10;
-	public static int meteorSpawnHeight = 300;
+	public static int meteorSpawnHeight = 200;
 	public static int meteorSizeRadius = 5;
 	public static float explosionPower = 10;
 	public static World enabledWorld = Bukkit.getServer().getWorld("world");
