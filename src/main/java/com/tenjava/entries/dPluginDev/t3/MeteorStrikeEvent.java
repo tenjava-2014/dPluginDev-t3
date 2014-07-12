304293d21db245ce6360a55723e795993d77c294
dPluginDev <mchungerdeans@gmail.com>
2014-07-12 14:53:21 -0700

    Just stuff

--- src/main/java/com/tenjava/entries/dPluginDev/t3/MeteorStrikeEvent.java
+++ src/main/java/com/tenjava/entries/dPluginDev/t3/MeteorStrikeEvent.java
@@ -21,7 +21,7 @@ public class MeteorStrikeEvent {
 	    	loc.getBlock().setType(Material.GRAVEL);
 	    }
 	    
-	    Location explodeLoc = new Location(Config.enabledWorld, meteorX, meteorCenter.getBlockY()-1, meteorZ);
+	    Location explodeLoc = new Location(Config.enabledWorld, meteorX, meteorCenter.getBlockY()-3, meteorZ);
 	    
 	    while (!explodeLoc.getBlock().getType().isSolid()){
 	    	explodeLoc.add(0, -1, 0);
